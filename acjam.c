71bc00c42ea6efd2804b738a8f8910f8200fe6f7
Felipe Gil <fgil90@gmail.com>
2024-02-29 22:41:23 +0000

    First Commit

---
+++ acjam.c
@@ -0,0 +1,15 @@
+#include "raylib.h"
+
+#define DRAW_FPS 60
+
+int main()
+{
+    SetTargetFPS(DRAW_FPS);
+
+    while (!WindowShouldClose())
+    {
+        
+    }
+    CloseWindow(); 
+    return 0;
+}
